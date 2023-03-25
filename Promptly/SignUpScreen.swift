d2097be5de6a547f896573e8e1363eb8bfd099eb
nihalkurki <nihalkurki@gmail.com>
2023-03-25 15:30:01 -0400

    3 distinct screens

--- Promptly/SignUpScreen.swift
+++ Promptly/SignUpScreen.swift
@@ -4,5 +4,142 @@
 //
 //  Created by Nihal Kurki on 3/22/23.
 //
+import UIKit
+import Firebase
 
-import Foundation
+class SignUpViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private let emailTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Email"
+        textField.keyboardType = .emailAddress
+        textField.autocapitalizationType = .none
+        textField.autocorrectionType = .no
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let passwordTextField: UITextField = {
+        let textField = UITextField()
+        textField.placeholder = "Password"
+        textField.textContentType = .oneTimeCode
+        textField.isSecureTextEntry = true
+        textField.textContentType = .init(rawValue: "")
+        textField.borderStyle = .roundedRect
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    private let signUpButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("Sign Up", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .blue
+        button.layer.cornerRadius = 5
+        button.addTarget(self, action: #selector(handleSignUp), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    // MARK: - View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+        
+        // Add subviews
+        view.addSubview(emailTextField)
+        view.addSubview(passwordTextField)
+        view.addSubview(signUpButton)
+        
+        // Set up constraints
+        NSLayoutConstraint.activate([
+            emailTextField.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            emailTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            emailTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            emailTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            passwordTextField.topAnchor.constraint(equalTo: emailTextField.bottomAnchor, constant: 20),
+            passwordTextField.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            passwordTextField.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            passwordTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            signUpButton.topAnchor.constraint(equalTo: passwordTextField.bottomAnchor, constant: 20),
+            signUpButton.leadingAnchor.constraint(equalTo: emailTextField.leadingAnchor),
+            signUpButton.trailingAnchor.constraint(equalTo: emailTextField.trailingAnchor),
+            signUpButton.heightAnchor.constraint(equalToConstant: 40),
+        ])
+    }
+    
+    // MARK: - Actions
+    
+    @objc private func handleSignUp() {
+        guard let email = emailTextField.text, !email.isEmpty else {
+            showAlert(with: "Error", message: "Please enter an email address")
+            return
+        }
+        
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(with: "Error", message: "Please enter a password")
+            return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+            if let error = error {
+                self.showAlert(with: "Error", message: error.localizedDescription)
+                return
+            }
+            
+            // User created successfully, do something here
+            
+            
+            //self.performSegue(withIdentifier: "SignUpSuccessSegue", sender: self)
+            // Present a different screen
+//            let signUpScreen = UIViewController()
+//            signUpScreen.view.backgroundColor = .white
+//            present(signUpScreen, animated: true, completion: nil)
+            
+            
+//            self.emailTextField.isHidden = true
+//            self.passwordTextField.isHidden = true
+//            self.signUpButton.isHidden = true
+            
+            let redVC = RedViewController()
+            redVC.modalPresentationStyle = .fullScreen
+            self.present(redVC, animated: true, completion: nil)
+            //self.navigationController?.pushViewController(redVC, animated: true)
+            
+//            let redVC = UIViewController()
+//            redVC.view.backgroundColor = UIColor.red
+//            // push the new view controller onto the navigation stack
+//            DispatchQueue.main.async {
+//                self.navigationController?.pushViewController(redVC, animated: true)
+//            }
+            
+            //self.dismiss(animated: true, completion: nil)
+            
+            print("User signed up successfully")
+                        
+            // Perform segue to the SignUpSuccessViewController
+//            self.performSegue(withIdentifier: "SignUpSuccessSegue", sender: nil)
+            
+//            let redVC = RedViewController()
+//            self.pushViewController(redVC, animated: true)
+        }
+    }
+    
+    private func showAlert(with title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okAction)
+        present(alert, animated: true, completion: nil)
+    }
+    
+}
+
+
--- Promptly/ViewController.swift
+++ Promptly/ViewController.swift
@@ -6,13 +6,179 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "PromptlyLogo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    private let prompt: UILabel = {
+        let label = UILabel()
+        label.text = "Prompted!"
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let SignUpButton: UIButton = {
+        let SignUpButton = UIButton()
+        SignUpButton.backgroundColor = .systemGreen
+        SignUpButton.setTitleColor(.white, for: .normal)
+        SignUpButton.setTitle("Sign Up", for: .normal)
+        return SignUpButton
+    }()
+    
+    private let RespondButton: UIButton = {
+        let RespondButton = UIButton()
+        RespondButton.backgroundColor = .systemGreen
+        RespondButton.setTitleColor(.white, for: .normal)
+        RespondButton.setTitle("Respond", for: .normal)
+        return RespondButton
+    }()
+    
+    private let LogOutButton: UIButton = {
+        let LogOutButton = UIButton()
+        LogOutButton.backgroundColor = .systemGreen
+        LogOutButton.setTitleColor(.white, for: .normal)
+        LogOutButton.setTitle("Log Out", for: .normal)
+        return LogOutButton
+    }()
 
     override func viewDidLoad() {
-        super.viewDidLoad()
+        //super.viewDidLoad()
+        //view.backgroundColor = .systemRed
         // Do any additional setup after loading the view.
+        
+        
+        if (FirebaseAuth.Auth.auth().currentUser != nil) {
+            showLoginScreen()
+        }
+        
+        
+        
+        
+        super.viewDidLoad()
+        view.addSubview(SignUpButton)
+        view.addSubview(RespondButton)
+        view.addSubview(LogOutButton)
+        
+        // Set background color
+        view.backgroundColor = UIColor(red: 246/255, green: 247/255, blue: 254/255, alpha: 1.0)
+        
+        // Create a button
+        let signUpButton = UIButton()
+        signUpButton.setTitle("Sign Up", for: .normal)
+        signUpButton.setTitleColor(.white, for: .normal)
+        signUpButton.backgroundColor = .blue
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signUpButton)
+        
+        let respondButton = UIButton()
+        respondButton.setTitle("Respond", for: .normal)
+        respondButton.setTitleColor(.white, for: .normal)
+        respondButton.backgroundColor = .blue
+        respondButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(respondButton)
+        
+        respondButton.isHidden = true
+        
+        let signOutButton = UIButton()
+        signOutButton.setTitle("Sign Out", for: .normal)
+        signOutButton.setTitleColor(.white, for: .normal)
+        signOutButton.backgroundColor = .blue
+        signOutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(signOutButton)
+        
+        signOutButton.isHidden = false
+        
+        view.addSubview(imageView)
+        view.addSubview(prompt)
+                
+        // Add constraints to the button
+        NSLayoutConstraint.activate([
+            signUpButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signUpButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            signUpButton.widthAnchor.constraint(equalToConstant: 150),
+            signUpButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            respondButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            respondButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 100),
+            respondButton.widthAnchor.constraint(equalToConstant: 150),
+            respondButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            signOutButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            signOutButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 160),
+            signOutButton.widthAnchor.constraint(equalToConstant: 150),
+            signOutButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -250),
+            imageView.widthAnchor.constraint(equalToConstant: 200),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+
+            prompt.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 20),
+            prompt.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 40),
+            prompt.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -40),
+            prompt.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        // Add an action to the button
+        signUpButton.addTarget(self, action: #selector(signUpButtonTapped), for: .touchUpInside)
+        
+        if FirebaseAuth.Auth.auth().currentUser != nil {
+            respondButton.isHidden = false
+            signUpButton.isHidden = true
+        }
+        
+        respondButton.addTarget(self, action: #selector(respondButtonTapped), for: .touchUpInside)
+        
+        signOutButton.addTarget(self, action: #selector(signOutButtonTapped), for: .touchUpInside)
+        
+        
+        
     }
+    
+    @objc func signUpButtonTapped() {
+        // Present a different screen
+        let SignUpScreen = SignUpViewController()
+        SignUpScreen.modalPresentationStyle = .fullScreen
+        //SignUpScreen.view.backgroundColor = .white
+        present(SignUpScreen, animated: true, completion: nil)
+    }
+    
+    @objc func showLoginScreen() {
+        let redVC = RedViewController()
+        redVC.modalPresentationStyle = .fullScreen
+        self.present(redVC, animated: true, completion: nil)
+    }
+    
+    
+    @objc func respondButtonTapped() {
+        // Present a different screen
+        let Respond = UIViewController()
+        Respond.view.backgroundColor = .orange
+        Respond.modalPresentationStyle = .fullScreen
+        present(Respond, animated: true, completion: nil)
+    }
+    
+    @objc func signOutButtonTapped() {
+        do {
+            try FirebaseAuth.Auth.auth().signOut()
+            
+        }
+        catch {
+            print("An Error While Signing Out Occured")
+        }
+        
+    }
+    
 
 
 }
